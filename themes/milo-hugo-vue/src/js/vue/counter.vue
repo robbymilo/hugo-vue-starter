1a1df8761f21144809597d9ea2fc7748f88c2dd8
Robby Milo <RobbyMilo@truckstop.com>
2019-01-02 15:34:06 -0700

    clean dist folder

--- themes/milo-hugo-vue/src/js/vue/counter.vue
+++ themes/milo-hugo-vue/src/js/vue/counter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="example">
-    Total: {{ msg }}
+    Total Counter Test: {{ msg }}
     <button @click="increase">Increase</button>
   </div>
 </template>
--- themes/milo-hugo-vue/src/webpack.config.js
+++ themes/milo-hugo-vue/src/webpack.config.js
@@ -3,7 +3,8 @@ const WriteFilePlugin = require('write-file-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const AssetsPlugin = require('assets-webpack-plugin');
 const autoprefixer = require('autoprefixer');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -11,10 +12,10 @@ module.exports = {
     app: './js/main.js',
   },
   output: {
-    path: path.join(__dirname, './../static/dist'),
+    path: path.resolve(__dirname, './../static/dist'),
     chunkFilename: `[id].[chunkhash].js`,
     filename:`[id].[chunkhash].js`,
-    publicPath:  path.join(__dirname, './../static/dist')
+    publicPath:  path.resolve(__dirname, './../static/dist')
   },
   optimization: {
     namedChunks: true,
@@ -86,6 +87,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist/*'], {
+      root: path.resolve(__dirname, './../static'),
+      verbose: true,
+      dry: false,
+    }),
     new WriteFilePlugin(),
     new MiniCssExtractPlugin({
       filename: "app.[contenthash].css",
